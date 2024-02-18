984643d9b58481b9459ead9461f44ef12e20f99e
Jimmy Patel <jimmypatel678@yahoo.com>
2024-02-17 19:01:21 -0500

    Font style chenaged

--- src/Components/Blog/BlogPost.jsx
+++ src/Components/Blog/BlogPost.jsx
@@ -1,10 +1,7 @@
-const BlogPost = ({ title, video, content, link }) => {
+const BlogPost = ({ title, image, content, link }) => {
   return (
     <div className="mb-4 md:mb-8 flex flex-col items-start">
-      <video autoPlay muted loop className="w-full h-full mb-4">
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <img src={image} alt={title} className="w-50 h-50 object-cover mb-4" />
       <h3 className="text-xl mb-2">{title}</h3>
       <p className="mb-4">{content}</p>
       <a href={link} className="text-orange-300 hover:underline block">
@@ -14,4 +11,4 @@ const BlogPost = ({ title, video, content, link }) => {
   );
 };
 
-export default BlogPost;
+export default BlogPost
