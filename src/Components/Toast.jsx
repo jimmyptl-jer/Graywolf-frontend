d7394c0b39763a39c062b62e7a468f54ef7dc7f1
Jimmy Patel <jimmypatel678@yahoo.com>
2024-02-24 19:51:01 -0500

    toast position modified

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -1,5 +1,6 @@
 import { useEffect, ReactPropTypes } from "react"
 
+
 const Toast = ({ message, type, onClose }) => {
 
   useEffect(() => {
@@ -12,13 +13,15 @@ const Toast = ({ message, type, onClose }) => {
     }
   }, [onClose])
 
-  const styles = type === "SUCCESS"
-    ? "fixed top-4 right-4 z-50 p-4 rounded-md bg-green-600 text-white max-w-md"
-    : "fixed top-4 right-4 z-50 p-4 rounded-md bg-red-600 text-white max-w-md"
+  const style =
+    type === "SUCCESS"
+      ? "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 p-4 rounded-md bg-green-500 text-white max-w-md transition-transform"
+      : "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 p-4 rounded-md bg-red-500 text-white max-w-md transition-transform";
+
 
 
   return (
-    <div className={styles}>
+    <div className={style}>
       <div className="flex justify-center items-center">
         <span className="text-lg font-semibold">
           {message}
