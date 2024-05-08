dd46e4e86f8aeef285bc61161d6c5d1721d0a94f
Jimmy Patel <jimmypatel678@yahoo.com>
2024-05-07 23:13:56 -0400

    Backend api call added

--- src/http.js
+++ src/http.js
@@ -1,6 +1,6 @@
-// const API_BASE_URL = "https://graywolg-api.onrender.com" || ''
+const API_BASE_URL = "https://graywolg-api.onrender.com" || 'http://localhost:3000'
 
-const API_BASE_URL = 'http://localhost:3000'
+// const API_BASE_URL = 'http://localhost:3000'
 
 
 export const contact = async (data) => {
