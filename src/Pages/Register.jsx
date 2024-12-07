5e44c8ecad222700fc3cd01a3ed5648de3a5d628
Jimmy Patel <jimmypatel678@yahoo.com>
2024-12-07 13:37:11 -0500

    Transfer to Auth0

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,14 @@
-import { Link } from 'react-router-dom'
-import RegisterForm from "../Forms/Login/RegisterForm"
-import GrayWolf from '../assets/logo.png';
+import { useAuth0 } from "@auth0/auth0-react";
+import GrayWolf from "../assets/logo.png";
 
 const Register = () => {
+  const { loginWithRedirect } = useAuth0();
 
+  const handleRegister = async () => {
+    await loginWithRedirect({
+      screen_hint: "signup", // Hint to show the signup page instead of login
+    });
+  };
 
   return (
     <div className="min-h-screen mt-10">
@@ -14,21 +19,28 @@ const Register = () => {
             alt="Logo"
             className="w-50 h-50 object-contain justify-center"
           />
-          <p className='text-center mt-2'>
+          <p className="text-center mt-2">
             Welcome to My Community!
-            <span className='block font-normal'>
-              Let create, inspire, and build a supportive community where ideas thrive. Welcome aboard!
+            <span className="block font-normal">
+              Let's create, inspire, and build a supportive community where ideas thrive. Welcome aboard!
             </span>
           </p>
         </div>
 
         <div className="flex-1">
-          <RegisterForm />
-          <div className='flex gap-2 text-sm mt-5'>
+          <div className="flex flex-col gap-4">
+            <button
+              onClick={handleRegister}
+              className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
+            >
+              Register with Auth0
+            </button>
+          </div>
+          <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/login' className='text-blue-500'>
+            <a href="/login" className="text-blue-500">
               Login
-            </Link>
+            </a>
           </div>
         </div>
       </div>
